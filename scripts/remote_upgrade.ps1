e96e4f63db509cc3bd93587c9a2eca5f4a7d648c
huadong <159921208@qq.com>
2025-12-28 14:53:53 +0800

    fix(cli): add gh-proxy failover and remove private scripts

--- scripts/remote_upgrade.ps1
+++
@@ -1,5 +0,0 @@
-$Server = "root@139.155.77.203"
-Write-Host ">>> Connecting to SupaCloud Server ($Server)..."
-Write-Host ">>> Executing 'supacloud upgrade'..."
-
-ssh -t $Server "source /etc/profile; supacloud upgrade"
--- scripts/remote_upgrade.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SERVER="root@139.155.77.203"
-echo ">>> Connecting to SupaCloud Server ($SERVER)..."
-echo ">>> Executing 'supacloud upgrade'..."
-
-ssh -t $SERVER "source /etc/profile; supacloud upgrade"
