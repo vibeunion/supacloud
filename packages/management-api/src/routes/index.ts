8b1eadb72d5cde4afa2064ba484b493bdd835ed5
huadong <159921208@qq.com>
2026-01-30 13:44:25 +0800

    feat(mgmt-api): add database monitoring and health check endpoints

--- packages/management-api/src/routes/index.ts
+++ packages/management-api/src/routes/index.ts
@@ -2,3 +2,4 @@ export { projectRoutes } from "./projects";
 export { organizationRoutes } from "./organizations";
 export { userRoutes } from "./user";
 export { backupRoutes } from "./backups";
+export { monitorRoutes } from "./monitor";
